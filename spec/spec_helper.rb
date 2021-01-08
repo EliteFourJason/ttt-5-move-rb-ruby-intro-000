f4a70fed76f69ff59f3f4f3c2a061ec5bdd88669
Jason Sevilla <jesevillaescobar@gmail.com>
2021-01-08 01:15:02 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,14 @@ def get_variable_from_file(file, variable)
     raise NameError, "local variable `#{variable}' not defined in #{file}."
   end
 end
+
+def capture_puts
+  begin
+    old_stdout = $stdout
+    $stdout = StringIO.new('','w')
+    yield
+    $stdout.string
+  ensure
+    $stdout = old_stdout
+  end
+end
